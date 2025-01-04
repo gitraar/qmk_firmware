5423c43d6d75325d86b958f6ccff42cb07c164c1
Rui Silva Freire <git@rvs.one>
2025-01-04 03:21:24 +0000

    begin making keymap.c prettier

--- keyboards/keebart/3w6_rgb/keymaps/gitraar/keymap.c
+++ keyboards/keebart/3w6_rgb/keymaps/gitraar/keymap.c
@@ -45,9 +45,29 @@ enum custom_keycodes {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-[_LAYER0] = LAYOUT_split_3x5_3(KC_Q, KC_W, KC_F, KC_P, KC_B, KC_J, KC_L, KC_U, KC_Y, KC_QUOT, LGUI_T(KC_A), LALT_T(KC_R), LCTL_T(KC_S), LSFT_T(KC_T), KC_G, KC_M, LSFT_T(KC_N), LCTL_T(KC_E), LALT_T(KC_I), LGUI_T(KC_O), KC_Z, RALT_T(KC_X), KC_C, KC_D, KC_V, KC_K, KC_H, KC_COMM, RALT_T(KC_DOT), KC_SLSH, LT(3,KC_ESC), LT(1,KC_SPC), LT(2,KC_TAB), LT(5,KC_ENT), LT(4,KC_BSPC), LT(6,KC_DEL)),
+[_LAYER0] = LAYOUT_split_3x5_3(
+    //|——————————————|——————————————|——————————————|——————————————|——————————————|      |——————————————|——————————————|——————————————|——————————————|——————————————|
+       KC_Q,          KC_W,          KC_F,          KC_P,          KC_B,                 KC_J,          KC_L,          KC_U,          KC_Y,          KC_QUOT,
+    //|——————————————|——————————————|——————————————|——————————————|——————————————|      |——————————————|——————————————|——————————————|——————————————|——————————————|
+       LGUI_T(KC_A),  LALT_T(KC_R),  LCTL_T(KC_S),  LSFT_T(KC_T),  KC_G,                 KC_M,          LSFT_T(KC_N),  LCTL_T(KC_E),  LALT_T(KC_I),  LGUI_T(KC_O),
+    //|——————————————|——————————————|——————————————|——————————————|——————————————|      |——————————————|——————————————|——————————————|——————————————|——————————————|
+       KC_Z,          RALT_T(KC_X),  KC_C,          KC_D,          KC_V,                 KC_K,          KC_H,          KC_COMM,       RALT_T(KC_DOT),KC_SLSH,
+    //|——————————————|——————————————|——————————————|——————————————|——————————————|      |——————————————|——————————————|——————————————|——————————————|——————————————|
+                                     LT(3,KC_ESC),  LT(1,KC_SPC),  LT(2,KC_TAB),         LT(5,KC_ENT),  LT(4,KC_BSPC), LT(6,KC_DEL)
+    //|                             |——————————————|——————————————|——————————————|      |——————————————|——————————————|——————————————|
+),
 
-[_LAYER1] = LAYOUT_split_3x5_3(KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_AGIN, KC_UNDO, KC_CUT, KC_COPY, KC_PSTE, KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, KC_NO, KC_CAPS, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_NO, KC_RALT, KC_NO, KC_NO, KC_NO, KC_INS, KC_HOME, KC_PGDN, KC_PGUP, KC_END, KC_NO, KC_NO, KC_NO, KC_ENT, KC_BSPC, KC_DEL),
+[_LAYER1] = LAYOUT_split_3x5_3(
+    //|——————————————|——————————————|——————————————|——————————————|——————————————|      |——————————————|——————————————|——————————————|——————————————|——————————————|
+       KC_NO,         KC_NO,         KC_NO,         KC_NO,         KC_NO,                KC_AGIN,       KC_UNDO,       KC_CUT,        KC_COPY,       KC_PSTE,
+    //|——————————————|——————————————|——————————————|——————————————|——————————————|      |——————————————|——————————————|——————————————|——————————————|——————————————|
+       KC_LGUI,       KC_LALT,       KC_LCTL,       KC_LSFT,       KC_NO,                KC_CAPS,       KC_LEFT,       KC_DOWN,       KC_UP,         KC_RGHT,
+    //|——————————————|——————————————|——————————————|——————————————|——————————————|      |——————————————|——————————————|——————————————|——————————————|——————————————|
+       KC_NO,         KC_RALT,       KC_NO,         KC_NO,         KC_NO,                KC_INS,        KC_HOME,       KC_PGDN,       KC_PGUP,       KC_END,
+    //|——————————————|——————————————|——————————————|——————————————|——————————————|      |——————————————|——————————————|——————————————|——————————————|——————————————|
+                                     KC_NO,         KC_NO,         KC_NO,                KC_ENT,        KC_BSPC,       KC_DEL
+    //|                             |——————————————|——————————————|——————————————|      |——————————————|——————————————|——————————————|
+),
 
 [_LAYER2] = LAYOUT_split_3x5_3(KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, KC_NO, KC_NO, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, KC_NO, KC_RALT, KC_NO, KC_NO, KC_NO, KC_NO, KC_WH_L, KC_WH_D, KC_WH_U, KC_WH_R, KC_NO, KC_NO, KC_NO, KC_BTN2, KC_BTN1, KC_BTN3),
 
